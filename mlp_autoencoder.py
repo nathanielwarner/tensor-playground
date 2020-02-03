478f6e81d12e6b53467c3b3215e0d18887fdfcdc
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-02-03 00:36:55 -0600

    Working!

--- mlp_autoencoder.py
+++ mlp_autoencoder.py
@@ -5,30 +5,39 @@ import random
 
 class MLPAutoEncoder(object):
     def __init__(self, input_dim, code_dim):
-        hidden_dim = int((input_dim + code_dim) / 2)
+        hidden_1_dim = 512
+        hidden_2_dim = 128
         self._params = {
-            "enc_W0": tf.Variable(tf.random.normal((hidden_dim, input_dim))),
-            "enc_b0": tf.Variable(tf.zeros((hidden_dim, 1))),
-            "enc_W1": tf.Variable(tf.random.normal((code_dim, hidden_dim))),
-            "enc_b1": tf.Variable(tf.zeros((code_dim, 1))),
-            "dec_W0": tf.Variable(tf.random.normal((hidden_dim, code_dim))),
-            "dec_b0": tf.Variable(tf.zeros((hidden_dim, 1))),
-            "dec_W1": tf.Variable(tf.random.normal((input_dim, hidden_dim))),
-            "dec_b1": tf.Variable(tf.zeros((input_dim, 1))),
+            "enc_W0": tf.Variable(tf.random.normal((hidden_1_dim, input_dim), mean=0.0, stddev=0.05)),
+            "enc_b0": tf.Variable(tf.zeros((hidden_1_dim, 1))),
+            "enc_W1": tf.Variable(tf.random.normal((hidden_2_dim, hidden_1_dim), mean=0.0, stddev=0.05)),
+            "enc_b1": tf.Variable(tf.zeros((hidden_2_dim, 1))),
+            "enc_W2": tf.Variable(tf.random.normal((code_dim, hidden_2_dim), mean=0.0, stddev=0.05)),
+            "enc_b2": tf.Variable(tf.zeros((code_dim, 1))),
+            "dec_W0": tf.Variable(tf.random.normal((hidden_2_dim, code_dim), mean=0.0, stddev=0.05)),
+            "dec_b0": tf.Variable(tf.zeros((hidden_2_dim, 1))),
+            "dec_W1": tf.Variable(tf.random.normal((hidden_1_dim, hidden_2_dim), mean=0.0, stddev=0.05)),
+            "dec_b1": tf.Variable(tf.zeros((hidden_1_dim, 1))),
+            "dec_W2": tf.Variable(tf.random.normal((input_dim, hidden_1_dim), mean=0.0, stddev=0.05)),
+            "dec_b2": tf.Variable(tf.zeros((input_dim, 1))),
         }
 
     def encode(self, inputs):
-        hidden_layer = tf.nn.leaky_relu(tf.matmul(self._params["enc_W0"], inputs, transpose_b=True)
-                                        + self._params["enc_b0"])
-        encoded = tf.transpose(tf.nn.sigmoid(tf.matmul(self._params["enc_W1"], hidden_layer)
-                                             + self._params["enc_b1"]))
+        hidden_layer_1 = tf.nn.leaky_relu(tf.matmul(self._params["enc_W0"], inputs, transpose_b=True)
+                                          + self._params["enc_b0"])
+        hidden_layer_2 = tf.nn.leaky_relu(tf.matmul(self._params["enc_W1"], hidden_layer_1)
+                                          + self._params["enc_b1"])
+        encoded = tf.transpose(tf.matmul(self._params["enc_W2"], hidden_layer_2)
+                               + self._params["enc_b2"])
         return encoded
 
     def decode(self, encoded):
-        hidden_layer = tf.nn.leaky_relu(tf.matmul(self._params["dec_W0"], encoded, transpose_b=True)
-                                        + self._params["dec_b0"])
-        decoded = tf.transpose(tf.nn.sigmoid(tf.matmul(self._params["dec_W1"], hidden_layer)
-                                             + self._params["dec_b1"]))
+        hidden_layer_1 = tf.nn.leaky_relu(tf.matmul(self._params["dec_W0"], encoded, transpose_b=True)
+                                          + self._params["dec_b0"])
+        hidden_layer_2 = tf.nn.leaky_relu(tf.matmul(self._params["dec_W1"], hidden_layer_1)
+                                          + self._params["dec_b1"])
+        decoded = tf.transpose(tf.nn.sigmoid(tf.matmul(self._params["dec_W2"], hidden_layer_2)
+                                             + self._params["dec_b2"]))
         return decoded
 
     def loss(self, inputs):
@@ -37,24 +46,21 @@ class MLPAutoEncoder(object):
         mean_square_error = tf.reduce_mean(tf.losses.mean_squared_error(inputs, decoded))
         return mean_square_error
 
-    def _training_step(self, inputs, learning_rate):
+    def _training_step(self, inputs, optimizer):
         with tf.GradientTape() as t:
             current_loss = self.loss(inputs)
         grads = t.gradient(current_loss, self._params)
-        for param in self._params.keys():
-            self._params[param].assign_sub(learning_rate * grads[param])
+        optimizer.apply_gradients(zip(grads.values(), self._params.values()))
 
-    def train(self, inputs, val_inputs, num_epochs, batch_size, learning_rate):
+    def train(self, inputs, val_inputs, num_epochs, batch_size, optimizer):
         for epoch_num in range(1, num_epochs + 1):
             for batch_num in range(int(inputs.shape[0] / batch_size)):
                 inputs_batch = inputs[batch_num * batch_size: batch_num * batch_size + batch_size]
-                self._training_step(inputs_batch, learning_rate)
+                self._training_step(inputs_batch, optimizer)
             train_loss = self.loss(inputs)
             val_loss = self.loss(val_inputs)
             print("Epoch {} of {} completed, training loss = {}, validation loss = {}".format(
                 epoch_num, num_epochs, train_loss, val_loss))
-            if epoch_num % (num_epochs / 4) == 0:
-                learning_rate /= 2
             if val_loss - train_loss > train_loss / 32:
                 print("Stopped because validation loss significantly exceeds training loss")
                 break
@@ -82,11 +88,11 @@ def main():
     x_train = tf.reshape(x_train, (len(x_train), input_dim))
     x_test = tf.reshape(x_test, (len(x_test), input_dim))
 
-    hidden_code_dim = 256
+    hidden_code_dim = 16
     model = MLPAutoEncoder(input_dim, hidden_code_dim)
-    model.train(x_train, x_test, 200, 128, 0.25)
+    model.train(x_train, x_test, 64, 1024, tf.keras.optimizers.Adam())
 
-    for _ in range(25):
+    for _ in range(4):
         plt.subplot(1, 3, 1)
         plt.title("Input Image")
         test_case = x_test[random.randrange(x_test.shape[0])]
@@ -96,7 +102,7 @@ def main():
         plt.subplot(1, 3, 2)
         plt.title("Hidden Representation")
         encoded = model.encode([test_case])
-        encoded_img = tf.reshape(encoded, (1, 16, 16))[0] * 255.0
+        encoded_img = tf.reshape(tf.nn.sigmoid(encoded), (1, 4, 4))[0] * 255.0
         plt.imshow(encoded_img, cmap='Greys')
 
         plt.subplot(1, 3, 3)
